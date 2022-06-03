854c963035c2e6246d39db804af2f70c1ad2a099
Alexandr Ksenz <AlexandrKsenz@gmail.com>
2022-06-03 14:19:27 +0200

    main

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ $(document).ready(function () {
         $('body').toggleClass('lock');
     })
 })
+
+window.onscroll = function showHeader() {
+    var header = document.querySelector('.header');
+    if(window.pageYOffset > 1){
+        header.classList.add('active');
+    } else{
+        header.classList.remove('active');
+    }
+}
